1195bbe6e4a273793b9a8c2de284cd7fae3ce5b7
Athan Massouras <amassouras@berkeley.edu>
2024-01-09 21:16:07 +0100

    legal moves: all except pawns

--- app/BoardRepresentation/LegalMoves.hs
+++ app/BoardRepresentation/LegalMoves.hs
@@ -3,39 +3,55 @@ module BoardRepresentation.LegalMoves where
     import BoardRepresentation.Bitboards
     import BoardRepresentation.Moves
 
-    getBitboardsOccupancy :: [(Bitboard, Piece)] -> Bitboard
-    getBitboardsOccupancy = foldr ((.|.) . fst) 0
-
     getBoardOccupancy :: Board -> Bitboard
-    getBoardOccupancy = getBitboardsOccupancy . getBoardBitboards
+    getBoardOccupancy = foldr ((.|.) . fst) 0 . getBoardBitboards
 
     -- Knights
+    fileMaskFull :: Int -> Bitboard
+    fileMaskFull squareI = 0x0101010101010101 `shiftL` (squareI .&. 7)
+
     knightMaskBB :: Bitboard -> Bitboard
     knightMaskBB knightBB = foldr (.|.) 0 [noNoEa, noEaEa, soEaEa, soSoEa, noNoWe, noWeWe, soWeWe, soSoWe]
         where
-            noNoEa = (knightBB `shiftL` 17) .&. complement (fileMask 7)
-            noEaEa = (knightBB `shiftL` 10) .&. complement (fileMask 7) .&. complement (fileMask 6)
-            soEaEa = (knightBB `shiftR` 6)  .&. complement (fileMask 7) .&. complement (fileMask 6)
-            soSoEa = (knightBB `shiftR` 15) .&. complement (fileMask 7)
-            noNoWe = (knightBB `shiftL` 15) .&. complement (fileMask 1)
-            noWeWe = (knightBB `shiftL` 6)  .&. complement (fileMask 1) .&. complement (fileMask 2)
-            soWeWe = (knightBB `shiftR` 10) .&. complement (fileMask 1) .&. complement (fileMask 2)
-            soSoWe = (knightBB `shiftR` 17) .&. complement (fileMask 1)
+            noNoEa = (knightBB `shiftL` 15) .&. complement (fileMaskFull 7)
+            noEaEa = (knightBB `shiftL` 6)  .&. complement (fileMaskFull 7) .&. complement (fileMaskFull 6)
+            soEaEa = (knightBB `shiftR` 10) .&. complement (fileMaskFull 7) .&. complement (fileMaskFull 6)
+            soSoEa = (knightBB `shiftR` 17) .&. complement (fileMaskFull 7)
+            noNoWe = (knightBB `shiftL` 17) .&. complement (fileMaskFull 0)
+            noWeWe = (knightBB `shiftL` 10) .&. complement (fileMaskFull 0) .&. complement (fileMaskFull 1)
+            soWeWe = (knightBB `shiftR` 6)  .&. complement (fileMaskFull 0) .&. complement (fileMaskFull 1)
+            soSoWe = (knightBB `shiftR` 15) .&. complement (fileMaskFull 0)
 
     knightMask :: Int -> Bitboard
     knightMask squareI = foldr (.|.) 0 [noNoEa, noEaEa, soEaEa, soSoEa, noNoWe, noWeWe, soWeWe, soSoWe]
         where
-            noNoEa = (1 `shift` (squareI + 17)) .&. complement (fileMask 7)
-            noEaEa = (1 `shift` (squareI + 10)) .&. complement (fileMask 7) .&. complement (fileMask 6)
-            soEaEa = (1 `shift` (squareI - 6))  .&. complement (fileMask 7) .&. complement (fileMask 6)
-            soSoEa = (1 `shift` (squareI - 15)) .&. complement (fileMask 7)
-            noNoWe = (1 `shift` (squareI + 15)) .&. complement (fileMask 1)
-            noWeWe = (1 `shift` (squareI + 6))  .&. complement (fileMask 1) .&. complement (fileMask 2)
-            soWeWe = (1 `shift` (squareI - 10)) .&. complement (fileMask 1) .&. complement (fileMask 2)
-            soSoWe = (1 `shift` (squareI - 17)) .&. complement (fileMask 1)
+            noNoEa = (1 `shift` (squareI + 15)) .&. complement (fileMaskFull 7)
+            noEaEa = (1 `shift` (squareI + 6))  .&. complement (fileMaskFull 7) .&. complement (fileMaskFull 6)
+            soEaEa = (1 `shift` (squareI - 10)) .&. complement (fileMaskFull 7) .&. complement (fileMaskFull 6)
+            soSoEa = (1 `shift` (squareI - 17)) .&. complement (fileMaskFull 7)
+            noNoWe = (1 `shift` (squareI + 17)) .&. complement (fileMaskFull 0)
+            noWeWe = (1 `shift` (squareI + 10)) .&. complement (fileMaskFull 0) .&. complement (fileMaskFull 1)
+            soWeWe = (1 `shift` (squareI - 6))  .&. complement (fileMaskFull 0) .&. complement (fileMaskFull 1)
+            soSoWe = (1 `shift` (squareI - 15)) .&. complement (fileMaskFull 0)
 
     knightMaskSquare :: Square -> Bitboard
     knightMaskSquare = knightMask . squareToLocation
+
+    -- Kings
+    kingMaskBB :: Bitboard -> Bitboard
+    kingMaskBB bb = sideAttacks .|. (sideAttacks `shiftL` 8) .|. (sideAttacks `shiftR` 8)
+        where
+            sideAttacks = lAttack .|. bb .|. rAttack
+            lAttack = bb `shiftL` 1 .&. complement (fileMaskFull 7)
+            rAttack = bb `shiftR` 1 .&. complement (fileMaskFull 0)
+
+    kingMask :: Int -> Bitboard
+    kingMask squareI = sideAttacks .|. (sideAttacks `shiftL` 8) .|. (sideAttacks `shiftR` 8)
+        where
+            sideAttacks = lAttack .|. (1 `shiftL` squareI) .|. rAttack
+            lAttack = 1 `shift` (squareI - 1) .&. complement (fileMaskFull 7)
+            rAttack = 1 `shift` (squareI + 1) .&. complement (fileMaskFull 0)
+
 
     -- Generate sliding piece attacks (classical approach, magic bitboards are a nightmare)
     -- We start with open board sliding attack bitboards
